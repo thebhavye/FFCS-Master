5a584f7f39934994eb262538d41c6cdf8f366435
thebhavye <bhavyekathuria@gmail.com>
2026-04-13 21:41:42 +0530

    Fixed some indentation issues

--- backend/app.py
+++ backend/app.py
@@ -264,6 +264,7 @@ def parse_regex_output(output_text):
     return combos, message_lines
 
 
+
+
 if __name__ == "__main__":
- 
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
